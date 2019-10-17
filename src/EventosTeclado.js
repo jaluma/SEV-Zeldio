6d8a624b7e7d941ce905c78c58625a7f4497226b
jaluma <javiermartinezalvarez98@gmail.com>
2019-10-17 22:31:56 +0200

    initial commit

---
+++ src/EventosTeclado.js
@@ -0,0 +1,78 @@
+// player 1
+var oneKey = 49
+var twoKey = 50
+
+var spaceKey = 32
+
+var upKey = 38
+var downKey = 40
+var leftKey = 37
+var rightKey = 39
+
+var teclas = [];
+
+window.addEventListener('keydown', onKeyDown, false);
+window.addEventListener('keyup', onKeyUp, false);
+
+function onKeyDown(event) {
+    entrada = entradas.teclado;
+
+    // agregar la tecla pulsada si no estaba
+    var posicion = teclas.indexOf(event.keyCode);
+    if (posicion == -1) {
+        teclas.push(event.keyCode);
+        switch (event.keyCode) {
+            case spaceKey:
+                controles.atacar = true;
+                controles.continuar = true;
+                break;
+            case upKey:
+                controles.moverY = -1;
+                break;
+            case downKey:
+                controles.moverY = 1;
+                break;
+            case rightKey:
+                controles.moverX = 1;
+                break;
+            case leftKey:
+                controles.moverX = -1;
+                break;
+        }
+
+    }
+
+}
+
+function onKeyUp(event) {
+    // sacar la tecla pulsada
+    var posicion = teclas.indexOf(event.keyCode);
+    teclas.splice(posicion, 1);
+    switch (event.keyCode) {
+        case spaceKey:
+            controles.atacar = false;
+            controles.continuar = false;
+            break;
+        case upKey:
+            if (controles.moverY == -1) {
+                controles.moverY = 0;
+            }
+            break;
+        case downKey:
+            if (controles.moverY == 1) {
+                controles.moverY = 0;
+            }
+            break;
+        case rightKey:
+            if (controles.moverX == 1) {
+                controles.moverX = 0;
+            }
+            break;
+        case leftKey:
+            if (controles.moverX == -1) {
+                controles.moverX = 0;
+            }
+            break;
+    }
+
+}
---
+++ src/layers/GameLayer.js
@@ -0,0 +1,235 @@
+class GameLayer extends Layer {
+
+    constructor() {
+        super();
+
+        this.resolution = new Object();
+        this.resolution.width = 480;
+        this.resolution.height = 320;
+
+        this.iniciar();
+    }
+
+    iniciar() {
+        this.espacio = new Espacio(0);
+
+        this.botonSalto = new Boton(imagenes.boton_salto, this.resolution.width * 0.9, this.resolution.height * 0.55);
+        this.botonDisparo = new Boton(imagenes.boton_disparo, this.resolution.width * 0.75, this.resolution.height * 0.83);
+        this.pad = new Pad(this.resolution.width * 0.14, this.resolution.height * 0.8);
+
+        this.scrollX = 0;
+        this.bloques = [];
+
+        this.fondo = new Fondo(imagenes.fondo_2, this.resolution.width * 0.5, this.resolution.height * 0.5);
+
+        this.enemigos = [];
+
+
+        this.fondoPuntos =
+            new Fondo(imagenes.icono_puntos, this.resolution.width * 0.85, this.resolution.height * 0.05);
+
+        this.puntos = new Texto(0, this.resolution.width * 0.9, this.resolution.height * 0.07);
+        this.cargarMapa("res/" + nivelActual + ".txt");
+    }
+
+    actualizar() {
+        this.espacio.actualizar();
+
+        // elementos fuera
+        // Enemigos muertos fuera del juego
+        for (var j = 0; j < this.enemigos.length; j++) {
+            if (this.enemigos[j] != null &&
+                this.enemigos[j].estado == estados.muerto) {
+
+                this.espacio.eliminarCuerpoDinamico(this.enemigos[j]);
+
+                this.enemigos.splice(j, 1);
+                j = j - 1;
+            }
+        }
+
+        this.jugador.actualizar();
+        for (var i = 0; i < this.enemigos.length; i++) {
+            this.enemigos[i].actualizar();
+        }
+
+        // colisiones
+        for (var i = 0; i < this.enemigos.length; i++) {
+            if (this.jugador.colisiona(this.enemigos[i])) {
+                this.iniciar();
+            }
+        }
+
+    }
+
+    calcularScroll() {
+        this.scrollX = this.jugador.x - this.resolution.width / 2;
+        this.scrollY = this.jugador.y - this.resolution.height / 2;
+    }
+
+
+    dibujar() {
+        this.calcularScroll();
+
+        this.fondo.dibujar();
+        for (var i = 0; i < this.bloques.length; i++) {
+            this.bloques[i].dibujar(this.scrollX, this.scrollY);
+        }
+
+        this.jugador.dibujar(this.scrollX, this.scrollY);
+        for (var i = 0; i < this.enemigos.length; i++) {
+            this.enemigos[i].dibujar(this.scrollX, this.scrollY);
+        }
+
+        // HUD
+        this.fondoPuntos.dibujar();
+        this.puntos.dibujar();
+
+        // if (!this.pausa && entrada == entradas.pulsaciones) {
+        //     this.botonDisparo.dibujar();
+        //     this.botonSalto.dibujar();
+        //     this.pad.dibujar();
+        // }
+
+    }
+
+
+    procesarControles() {
+        // disparar
+        if (controles.atacar) {
+            var nuevoAtaque = this.jugador.atacar;
+            if (nuevoAtaque != null) {
+                this.espacio.agregarCuerpoDinamico(nuevoAtaque);
+                // hacer algo con el ataque
+            }
+        }
+
+        // Eje X
+        if (controles.moverX > 0) {
+            this.jugador.moverX(1);
+        } else if (controles.moverX < 0) {
+            this.jugador.moverX(-1);
+        } else {
+            this.jugador.moverX(0);
+        }
+
+        // Eje Y
+        if (controles.moverY > 0) {
+            this.jugador.moverY(1)
+        } else if (controles.moverY < 0) {
+            this.jugador.moverY(-1)
+        } else {
+            this.jugador.moverY(0)
+        }
+
+    }
+
+
+    cargarMapa(ruta) {
+        var fichero = new XMLHttpRequest();
+        fichero.open("GET", ruta, false);
+
+        fichero.onreadystatechange = function() {
+            var texto = fichero.responseText;
+            var lineas = texto.split('\n');
+            this.anchoMapa = (lineas[0].length - 1) * 40;
+            this.altoMapa = (lineas.length - 1) * 40;
+            for (var i = 0; i < lineas.length; i++) {
+                var linea = lineas[i].split(' ');
+
+                for (var j = 0; j < linea.length; j++) {
+                    var simbolo = linea[j];
+                    var x = 40 / 2 + j * 40; // x central
+                    var y = 32 + i * 32; // y de abajo
+                    this.cargarObjetoMapa(simbolo, x, y);
+                }
+            }
+        }.bind(this);
+
+        fichero.send(null);
+    }
+
+    cargarObjetoMapa(simbolo, x, y) {
+        switch (simbolo) {
+            case "C":
+                this.copa = new Bloque(imagenes.copa, x, y);
+                this.copa.y = this.copa.y - this.copa.alto / 2;
+                // modificación para empezar a contar desde el suelo
+                this.espacio.agregarCuerpoDinamico(this.copa);
+                break;
+
+            case "E":
+                var enemigo = new Enemigo(x, y);
+
+                enemigo.y = enemigo.y - enemigo.alto / 2;
+                // modificación para empezar a contar desde el suelo
+                this.enemigos.push(enemigo);
+                this.espacio.agregarCuerpoDinamico(enemigo);
+                break;
+            case "1":
+                this.jugador = new Jugador(x, y);
+                // modificación para empezar a contar desde el suelo
+                this.jugador.y = this.jugador.y - this.jugador.alto / 2;
+                this.espacio.agregarCuerpoDinamico(this.jugador);
+        }
+
+        // añadimos los bloques
+        switch (simbolo) {
+            default: var bloque = new Bloque(imagenes["cesped_basico_" + parseInt(Math.random() * lengthCesped)], x, y);
+            bloque.y = bloque.y - bloque.alto / 2;
+            // modificación para empezar a contar desde el suelo
+            this.bloques.push(bloque);
+            break;
+        }
+    }
+
+    calcularPulsaciones(pulsaciones) {
+        // Suponemos botones no estan pulsados
+        this.botonDisparo.pulsado = false;
+        this.botonSalto.pulsado = false;
+
+        // suponemos que el pad está sin tocar
+        controles.moverX = 0;
+        controles.moverY = 0;
+
+        for (var i = 0; i < pulsaciones.length; i++) {
+            if (this.pad.contienePunto(pulsaciones[i].x, pulsaciones[i].y)) {
+                var orientacionX = this.pad.obtenerOrientacionX(pulsaciones[i].x);
+                if (orientacionX > 20) { // de 0 a 20 no contabilizamos
+                    controles.moverX = 1;
+                }
+                if (orientacionX < -20) { // de -20 a 0 no contabilizamos
+                    controles.moverX = -1;
+                }
+            }
+
+
+            if (this.botonDisparo.contienePunto(pulsaciones[i].x, pulsaciones[i].y)) {
+                this.botonDisparo.pulsado = true;
+                if (pulsaciones[i].tipo == tipoPulsacion.inicio) {
+                    controles.disparo = true;
+                }
+            }
+
+            if (this.botonSalto.contienePunto(pulsaciones[i].x, pulsaciones[i].y)) {
+                this.botonSalto.pulsado = true;
+                if (pulsaciones[i].tipo == tipoPulsacion.inicio) {
+                    controles.moverY = 1;
+                }
+            }
+
+        }
+
+        // No pulsado - Boton Disparo
+        if (!this.botonDisparo.pulsado) {
+            controles.disparo = false;
+        }
+
+        // No pulsado - Boton Salto
+        if (!this.botonSalto.pulsado) {
+            controles.moverY = 0;
+        }
+    }
+
+
+}
