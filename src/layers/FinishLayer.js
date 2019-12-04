42c62e58fa28d37a19ce5f608a3461b64e4a2a5c
Javier Martínez <javiermartinezalvarez98@gmail.com>
2019-12-04 16:51:02 +0100

    Añadir escena final

---
+++ src/layers/FinishLayer.js
@@ -0,0 +1,28 @@
+class FinishLayer extends Layer {
+
+    constructor() {
+        super();
+        this.iniciar();
+    }
+
+    iniciar() {
+        this.fondo = new Fondo(imagenes.menu_fondo, resolution.width * 0.5, resolution.height * 0.5);
+        this.texto = new TextoMenu("¡Felicidades! Has completado el juego.", resolution.width * 0.5, resolution.height * 0.7);
+        this.fondo2 = new Fondo(imagenes.fondo_2, resolution.width * 0.5, resolution.height * 0.5);
+    }
+
+    dibujar() {
+        this.fondo2.dibujar();
+        this.fondo.dibujar();
+        this.texto.dibujar();
+    }
+
+    procesarControles() {
+        // siguiente pantalla
+        if (controles.continuar) {
+            layer = new MenuLayer();
+            controles.continuar = false;
+        }
+    }
+
+}
